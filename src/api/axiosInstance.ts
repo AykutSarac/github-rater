27220b0453c12057ae06120a6b19b5a266d613c0
Ahmet ilhan <ailhan@hepsiemlak.com>
2022-12-11 17:13:17 +0300

    interceptor added for coneole tracking by end users

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,9 +1,12 @@
 import axios, { AxiosInstance } from 'axios';
+import { setupInterceptors } from './axiosInterceptors';
 
 const axiosInstance: AxiosInstance = axios.create({
   baseURL: 'https://api.github.com/users',
   method: 'GET',
   responseType: 'json',
 });
+
+setupInterceptors(axiosInstance);
 
 export default axiosInstance;
